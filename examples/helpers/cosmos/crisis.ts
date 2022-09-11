7c781fc6dbb827cd5c0609bf57d4847d805e4741
swarna1101 <113277972+swarna1101@users.noreply.github.com>
2022-09-12 03:35:06 +0530

    Update crisis.ts

--- examples/helpers/cosmos/crisis.ts
+++ examples/helpers/cosmos/crisis.ts
@@ -1,9 +1,47 @@
 import { CosmosChainClient, cosmos, cosmwasm, osmosis } from "cosmsdkjs";
 
-// Supported tranasction interfaces - types/proto/cosmos/crisis/v1beta1/tx.rpc.msg.d.ts
+// Supported tranasction interfaces - types/proto/cosmos/crisis/v1beta1/tx.rpc.msg.d.
 
+
+export interface MsgVerifyInvariant {
+	  type: "cosmos-sdk/MsgVerifyInvariant";
+	  value: {
+		sender: string;
+		invariant_module_name: string;
+		invariant_route: string;
+	  };
+			
+}
+
+export interface MsgVerifyInvariantResponse {
+	  height: string;
+	  result: {
+		events: cosmos.Event[];
+		log: string;
+	  };
+
+}
 
 
 //---------------------------------------------
 // COSMOS::CRISIS MODULE - TRANSACTION HELPERS
 // --------------------------------------------
+
+
+
+
+// MsgVerifyInvariant
+export function make_cosmos_crisis_verifyInvariant_msg(
+  sender: string,
+  invariant_module_name: string,
+  invariant_route: string,
+): cosmos.tx.Msg {
+  return {
+    typeUrl: "/cosmos.crisis.v1beta1.MsgVerifyInvariant",
+    value: {
+      sender: sender,
+      invariantModuleName: invariant_module_name,
+      invariantRoute: invariant_route,
+    },
+  };
+}
