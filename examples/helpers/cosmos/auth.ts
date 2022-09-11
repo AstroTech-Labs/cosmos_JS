9c8b746de5267d81cf6c8802c0caa653f5a61138
swarna1101 <113277972+swarna1101@users.noreply.github.com>
2022-09-11 23:30:54 +0530

    Update auth.ts

--- examples/helpers/cosmos/auth.ts
+++ examples/helpers/cosmos/auth.ts
@@ -1,7 +1,18 @@
 import { CosmosChainClient, cosmos, cosmwasm, osmosis } from "cosmsdkjs";
 
-// Supported query interfaces - types/proto/cosmos/auth/v1beta1/query.rpc.query.d.ts
+//Supported query interfaces - types/proto/cosmos/auth/v1beta1/query.rpc.query.d.ts
 
 //---------------------------------------------
 // COSMOS::AUTH MODULE - QUERY HELPERS
 // --------------------------------------------
+export async function query_cosmos_auth(client: CosmosChainClient) {
+	
+  
+  let response = await client.query.cosmos.auth.v1beta1.account(
+	cosmos.auth.v1beta1.QueryAccountRequest.fromPartial({ address: "cosmos1qyqsyqcyq5rqwzqfpg9scrgjl03hn0y2ydfh5u" })
+	  )
+	    return response;
+		
+				
+}
+  
