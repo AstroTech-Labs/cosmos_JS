5fa79e35e95894c7ef7a67c4666db9412d633447
swarna1101 <113277972+swarna1101@users.noreply.github.com>
2022-09-14 16:00:48 +0530

    add: functions in crisis.ts

--- examples/helpers/cosmos/crisis.ts
+++ examples/helpers/cosmos/crisis.ts
@@ -1,47 +1,22 @@
-import { CosmosChainClient, cosmos, cosmwasm, osmosis } from "cosmsdkjs";
+import { cosmos } from "cosmsdkjs";
 
 // Supported tranasction interfaces - types/proto/cosmos/crisis/v1beta1/tx.rpc.msg.d.
 
-
-export interface MsgVerifyInvariant {
-	  type: "cosmos-sdk/MsgVerifyInvariant";
-	  value: {
-		sender: string;
-		invariant_module_name: string;
-		invariant_route: string;
-	  };
-			
-}
-
-export interface MsgVerifyInvariantResponse {
-	  height: string;
-	  result: {
-		events: cosmos.Event[];
-		log: string;
-	  };
-
-}
 
 
 //---------------------------------------------
 // COSMOS::CRISIS MODULE - TRANSACTION HELPERS
 // --------------------------------------------
 
-
-
-
-// MsgVerifyInvariant
-export function make_cosmos_crisis_verifyInvariant_msg(
-  sender: string,
-  invariant_module_name: string,
-  invariant_route: string,
-): cosmos.tx.Msg {
-  return {
-    typeUrl: "/cosmos.crisis.v1beta1.MsgVerifyInvariant",
-    value: {
-      sender: sender,
-      invariantModuleName: invariant_module_name,
-      invariantRoute: invariant_route,
-    },
-  };
+export async function execute_cosmos_crisis_verify_invariant(
+    sender: string,
+    invariantModuleName: string,
+    invariantRoute: string,
+) {
+    let msg = cosmos.crisis.v1beta1.MsgVerifyInvariant.fromPartial({
+        sender: sender,
+        invariantModuleName: invariantModuleName,
+        invariantRoute: invariantRoute,
+    });
+    return msg;
 }
