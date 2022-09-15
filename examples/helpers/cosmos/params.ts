cec589025c31cb1beffb4d5b4e026cce78b7745d
swarna1101 <113277972+swarna1101@users.noreply.github.com>
2022-09-15 23:59:51 +0530

    Add: functions in params.ts

--- examples/helpers/cosmos/params.ts
+++ examples/helpers/cosmos/params.ts
@@ -1,7 +1,14 @@
-import { CosmosChainClient, cosmos, cosmwasm, osmosis } from "cosmsdkjs";
+import { CosmosChainClient, cosmos} from "cosmsdkjs";
 
 // Supported query interfaces - types/proto/mint/params/v1beta1/query.rpc.query.d.ts
 
 //---------------------------------------------
 // COSMOS::PARAMS MODULE - QUERY HELPERS
 // --------------------------------------------
+
+export async function query_cosmos_params_params(client: CosmosChainClient) {
+    let response = await client.query.cosmos.params.v1beta1.params(
+        cosmos.params.v1beta1.QueryParamsRequest.fromPartial({}),
+    );
+    return response;
+}
