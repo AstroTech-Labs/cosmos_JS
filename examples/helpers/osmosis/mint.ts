02ece64b4c46f17cbc5c1d5fdea14b6a7d91ac26
swarna1101 <113277972+swarna1101@users.noreply.github.com>
2022-09-16 16:43:03 +0530

    Add: functions in mint.ts

--- examples/helpers/osmosis/mint.ts
+++ examples/helpers/osmosis/mint.ts
@@ -1,4 +1,4 @@
-import { CosmosChainClient, cosmos, cosmwasm, osmosis } from "cosmsdkjs";
+import {CosmosChainClient, osmosis} from "cosmsdkjs";
 
 
 // Supported query interfaces - types/proto/osmosis/mint/v1beta1/query.rpc.query.d.ts
@@ -6,4 +6,19 @@ import { CosmosChainClient, cosmos, cosmwasm, osmosis } from "cosmsdkjs";
 //---------------------------------------------
 // OSMOSIS::MINT MODULE - QUERY HELPERS
 // --------------------------------------------
+
+export async function query_osmosis_mint_params(client: CosmosChainClient) {
+    let response = await client.query.osmosis.mint.v1beta1.params(
+        osmosis.mint.QueryParamsRequest.fromPartial({})
+    )
+    return response;
+}
+
+export async function query_osmosis_mint_epochProvisions(client: CosmosChainClient) {
+    let response = await client.query.osmosis.mint.v1beta1.epochProvisions(
+        osmosis.mint.QueryEpochProvisionsRequest.fromPartial({})
+    )
+    return response;
+}
+
 
