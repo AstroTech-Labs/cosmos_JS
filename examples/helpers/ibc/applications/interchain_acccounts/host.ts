e4250888d89a125c6f1937c64e37f80093f1cddd
swarna1101 <113277972+swarna1101@users.noreply.github.com>
2022-09-17 15:15:59 +0530

    Add : functions in host.ts

--- examples/helpers/ibc/applications/interchain_acccounts/host.ts
+++ examples/helpers/ibc/applications/interchain_acccounts/host.ts
@@ -1,7 +1,14 @@
-import { CosmosChainClient, cosmos, cosmwasm, osmosis } from "cosmsdkjs";
-
-
-
-
+import {CosmosChainClient, ibc} from "cosmsdkjs";
 
 // Supported query interfaces - types/proto/ibc/applications/interchain_accounts/host/v1/query.rpc.query.d.ts
+
+//---------------------------------------------
+// IBC::HOST MODULE - QUERY HELPERS
+// --------------------------------------------
+
+export async function query_ibc_applications_interchain_accounts_host_params(client: CosmosChainClient) {
+    let response = await client.query.ibc.applications.interchain_accounts.host.v1.params(
+        ibc.applications.interchain_accounts.host.v1.QueryParamsRequest.fromPartial({}),
+    );
+    return response;
+}
