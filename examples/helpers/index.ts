c6b498d172098e658abb408fd2cb4fd36f6fa572
swarna1101 <113277972+swarna1101@users.noreply.github.com>
2022-09-19 15:18:39 +0530

    Update: index.ts

--- examples/helpers/index.ts
+++ examples/helpers/index.ts
@@ -1,5 +1,5 @@
 export * as cosmosHelpers from "./cosmos";
 export * as osmosisHelpers from "./osmosis";
 export * as wasmHelpers from "./cosmwasm";
-// export * as ibcHelpers from "./ibc";
+export * as ibcHelpers from "./ibc";
 
